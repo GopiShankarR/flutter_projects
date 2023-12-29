e8c7fec24b061837f22e13a00820c72b35667ce6
GopiShankarR <gopi2605@gmail.com>
2023-12-28 20:53:55 -0600

    passenger choices pop up code module addition

---
+++ lib/views/passenger_choices.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PassengerChoices extends StatefulWidget {
+  const PassengerChoices({super.key});
+
+  @override
+  State<PassengerChoices> createState() => _PassengerChoicesState();
+}
+
+class _PassengerChoicesState extends State<PassengerChoices> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
